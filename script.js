13e045e4eb25adba48eb60fa4a358fa63d628bb0
Aurélien Roux <aureroux13@gmail.com>
2025-05-09 18:35:12 +0200

    add assets and create js images

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log("connect");
+/** @type {HTMLCanvasElement} */
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+
+const village = new Image();
+village.src = "village.png";
+
+const bear = new Image();
+bear.src = "bear.png";
