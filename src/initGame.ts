39c0c2763bf70773d62f167681bae76d9d20fc7c
Aurélien Roux <aureroux13@gmail.com>
2025-05-09 18:33:33 +0200

    restart vanilla

--- src/initGame.ts
+++
@@ -1,8 +0,0 @@
-import makeKaplayCtx from "./kaplayCtx";
-
-export default async function initGame() {
-  const k = makeKaplayCtx();
-
-  // eslint-disable-next-line no-console
-  console.log(`/src/initGame.ts l.6 k => =+=+=+=+=+=+=+=+=+=+=+=+=+`, k);
-}
