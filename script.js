2adfa6c0ff67f40a03c54c1207b12ec758f0923c
Aurélien Roux <aureroux13@gmail.com>
2025-05-09 18:36:32 +0200

    insure all images are loaded and draw to canvas

--- script.js
+++ script.js
@@ -7,3 +7,18 @@ village.src = "village.png";
 
 const bear = new Image();
 bear.src = "bear.png";
+
+/**
+ * @param {HTMLImageElement} image
+ * @returns {Promise<void>}
+ */
+function loadImage(image) {
+  return new Promise((res) => {
+    image.addEventListener("load", res);
+  });
+}
+
+Promise.all([loadImage(village), loadImage(bear)]).then(() => {
+  ctx.drawImage(village, -175, -400, 1600, 1600);
+  ctx.drawImage(bear, 1024 / 2 - 50, 761 / 2 - 50, 100, 100);
+});
