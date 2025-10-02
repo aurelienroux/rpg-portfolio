1f337955dabdd206f4af66d4df677d02de9b8090
Aurélien Roux <aureroux13@gmail.com>
2025-10-02 21:42:13 +0200

    update office texts

--- data/office-dialogs.js
+++ data/office-dialogs.js
@@ -10,29 +10,12 @@ const intro = new DialogBox({
     width: tileSizeInPixel,
     height: tileSizeInPixel,
   },
-  text: `
-    <p>
-      Welcome. My name is Aurelien Roux and I am a Front-end developer.
-    </p>
-    <p>
-      You can move around with ARROWS from the Office to the Gym, Interests or Projects room.
-      On mobile, click on blue buttons.
-    </p>
-    <p>
-      Interaction points <img src="../images/down-arrow.png" /> can be opened with SPACE key.
-      To close a dialog, press ESC.
-    </p>
-    <p>
-      If a dialog has choices <img src="../images/select-hand.png" />,
-      use UP and DOWN arrows to select and ENTER to confirm.
-    </p>
-    <p>
-      Press O to see my CV or tap top-left. For help, press H.
-    </p>
-    <p>
-      Thank you for visiting, I hope you enjoy it !
-    </p>
-  `,
+  text: `<p>Welcome! My name is Aurelien Roux and I am a software developer.</p>
+    <p>Move around with ARROWS or click on the arrows button on mobile.</p>
+    <p>Interaction points <img src="../images/down-arrow.png" /> can be opened and closed with SPACE key. To close a dialog, press ESC.</p>
+    <p>If a dialog has choices <img src="../images/select-hand.png" />, use UP and DOWN arrows to select and ENTER to confirm.</p>
+    <p>You can press O to see my CV directly. And press H For help.</p>
+    <p>Thank you for visiting, I hope you enjoy it!</p>`,
 });
 
 const diplomas = new DialogBox({
@@ -44,14 +27,9 @@ const diplomas = new DialogBox({
     width: tileSizeInPixel,
     height: tileSizeInPixel,
   },
-  text: `
-  <p>
-    My original background is in Business and Administration,
-    having studied in both France and Canada.
-  </p>
-  <p>
-    In 2012, I earned my MBA from HEC Montreal.
-  </p>`,
+  text: `<p>My background is in Business and Administration, with studies in France and Canada, and an MBA from HEC Montréal (2012).</p>
+    <p>This foundation gives me more than just coding skills. I build products with a strong understanding of
+    business strategy, user needs, and product vision.</p>`,
 });
 
 const code = new DialogBox({
@@ -63,13 +41,11 @@ const code = new DialogBox({
     width: tileSizeInPixel,
     height: tileSizeInPixel,
   },
-  text: `
-   A self-taught developer, I further strengthened my skills in 2017 by completing DecodeMTL’s
-   intensive 8 weeks full-stack JavaScript bootcamp.
-  `,
+  text: `<p>In 2017, I made the leap into web development as a front-end developer, completing Montreal’s DecodeMTL
+    full-stack JavaScript bootcamp and building on my self-taught foundation.</p>`,
 });
 
-const opquast = new DialogBox({
+const opquast = new DialogChoicesBox({
   position: {
     x: 1016,
     y: -275,
@@ -78,9 +54,14 @@ const opquast = new DialogBox({
     width: tileSizeInPixel,
     height: tileSizeInPixel,
   },
-  text: `
-    In 2022, I earned the Opquast certification (Open quality standards)
-    with a score of 910/1000, qualifying at the Expert level.`,
+  text: `<p>In 2022, I earned the Opquast certification (Open Quality Standards) with a score of 910/1000, qualifying at the Expert level.</p>
+    <p>Opquast is an internationally recognized standard for web quality and accessibility, covering best practices across performance, security, SEO, accessibility, and user experience.</p>`,
+  choices: [
+    {
+      displayText: "Open Opquast",
+      url: "https://www.opquast.com/en/",
+    },
+  ],
 });
 
 const agencies = new DialogChoicesBox({
@@ -92,10 +73,10 @@ const agencies = new DialogChoicesBox({
     width: tileSizeInPixel,
     height: tileSizeInPixel,
   },
-  text: `
-    I began my career as a front-end developer in 2017, working in digital agencies
-    where I contributed to projects for a wide range of clients across France and Canada.
-  `,
+  text: `<p>I started as a front-end developer in 2017 in digital agencies where I collaborated on diverse projects
+    for clients across France and Canada.</p>
+    <p>These experiences exposed me to diverse industries and challenges, strengthening my adaptability, teamwork,
+    and ability to deliver effective digital solutions.</p>`,
   choices: [
     {
       displayText: "Open CleverAge site",
@@ -117,22 +98,22 @@ const startups = new DialogChoicesBox({
     width: tileSizeInPixel,
     height: tileSizeInPixel,
   },
-  text: `
-    After agencies, I shifted to startup environments in 2019, taking on front-end developer roles
-    in companies ranging from pre-seed stage, to mature organizations with 500+ employees.
-  `,
+  text: `<p>In 2019, I moved from agencies to startups, working at companies ranging from early-stage ventures to 500+ employees.</p>
+    <p>This shift let me experience the full spectrum of product development, from building MVPs in lean teams
+    to contributing to scalable solutions in larger organizations.</p>`,
+
   choices: [
     {
-      displayText: "Open Busbud",
-      url: "https://www.busbud.com",
+      displayText: "Open AerialAI",
+      url: "https://aerial.ai",
     },
     {
       displayText: "Open AcquireApp",
       url: "https://www.crunchbase.com/organization/acquire-app",
     },
     {
-      displayText: "Open AerialAI",
-      url: "https://aerial.ai",
+      displayText: "Open Busbud",
+      url: "https://www.busbud.com",
     },
   ],
 });
@@ -147,10 +128,9 @@ const volunteering = new DialogChoicesBox({
     width: tileSizeInPixel,
     height: tileSizeInPixel,
   },
-  text: `
-    Since 2017, I have been developing and maintaining the website for FIKAS,
-    a Nordic cultural festival held in Montreal.
-  `,
+  text: `<p>Since 2017, I have been volunteering with Fikas, a Nordic cultural festival in Montreal,
+    where I created and continue to maintain the festival’s website to ensure it remains functional, accessible,
+    and up to date for attendees.</p>`,
   choices: [
     {
       displayText: "Open Fikasfest",
